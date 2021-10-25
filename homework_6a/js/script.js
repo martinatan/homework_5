3df33c6568550853f356aedd47d2215b23294e0e
martinatan <mltan368@gmail.com>
2021-10-25 00:51:15 -0400

    Update bag icon for all pages

---
+++ homework_6a/js/script.js
@@ -0,0 +1,14 @@
+/************
+ * JavaScript for general functionality
+ * of Muddy Paws Adventure Gear
+ * 
+ * Currently updates Bag icon with number
+ * of items in the Shopping Bag.
+*************/
+
+
+//get number of items in Bag from local storage
+
+
+//get bag-quantity id
+//insert number of items into bag-quantity
